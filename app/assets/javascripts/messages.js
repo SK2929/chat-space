7807e4e70a83eff5b4f8676848cba80f3365fbb9
SK2929 <sk@koyamashoutairanoMacBook-Pro.local>
2018-11-25 12:33:05 +0900

    Edit.message.js
    4. 2のイベントが発火したときにAjaxを使用して、messages#createが動くようにする

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,7 +1,16 @@
 $(function(){
   $('#new_message').on('submit', function(e){
     e.preventDefault();
-    console.log(this)
     var formData = new FormData(this);
+    var url = $(this).attr('action')
+    // 非同期通信に必要なオプションの設定
+    $.ajax({
+      url: href,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
   })
 })

b354c2dfa7cb8eb57b01eea6aa4f2348864aaead
SK2929 <sk@koyamashoutairanoMacBook-Pro.local>
2018-11-26 20:30:47 +0900

    Edit-message.js
    7. 返ってきたJSONをdoneメソッドで受取り、HTMLを作成する
    （doneメソッドで受け取る記述は、53行目〜56行目）
    （HTMLを作成する記述は2行目〜39行目）
    
    8. 7で作成したHTMLをメッセージ画面の一番下に追加する（55行目）
    
    9. HTMLを追加した分、メッセージ画面を下にスクロールする（57行目）
    
    10. 非同期に失敗した場合の処理も準備する（59、60行目）

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,42 @@
 $(function(){
+  function buildHTML(message){
+    if(message.content && message.image) {
+      var html = `<div class="message">
+                    <div class="upper-message">
+                      <div class="upper-message__user-name">${ message.user_name }</div>
+                      <div class="upper-message__date">${ message.created_at }</div>
+                    </div>
+                    <div class="lower-meesage">
+                      <p class="lower-message__content">${ message.content }</p>
+                      <img class="lower-message__image" src="${ message.image }">
+                    </div>
+                  </div>`
+      return html;
+    } else if (message.content) {
+    var html = `<div class="message">
+                  <div class="upper-message">
+                    <div class="upper-message__user-name">${ message.user_name }</div>
+                    <div class="upper-message__date">${ message.created_at }</div>
+                  </div>
+                  <div class="lower-meesage">
+                    <p class="lower-message__content">${ message.content }</p>
+                  </div>
+                </div>`
+    return html;
+    } else {
+    var html = `<div class="message">
+                  <div class="upper-message">
+                    <div class="upper-message__user-name">${ message.user_name }</div>
+                    <div class="upper-message__date">${ message.created_at }</div>
+                  </div>
+                  <div class="lower-meesage">
+                    <img class="lower-message__image" src="${ message.image }">
+                  </div>
+                </div>`
+    return html;
+    }
+  }
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -12,5 +50,18 @@ $(function(){
       processData: false,
       contentType: false
     })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html)
+      $('.form__message').val('')
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+    })
+    .fail(function(){
+      alert('error');
+    })
   })
 })
+
+
+
+

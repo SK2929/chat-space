618e86525a8b47ebdaffa1faba9f6de028b33a12
SK2929 <sk@koyamashoutairanoMacBook-Pro.local>
2018-11-28 15:02:27 +0900

    Edit-user.js
    4. テキストフィールドに入力されるたびにイベントが発火するようにする

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,2 +1,15 @@
-type: 'GET',
-url: '/users',
+$(function() {
+
+  // 非同期通信を起こす場所の指定
+  $(".chat-group-form__input").on("keyup", function() {
+    var input = $(".chat-group-form__input").val();
+    console.log(input)
+    // Ajaxで非同期通信に必要なオプションの指定
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+  });
+});

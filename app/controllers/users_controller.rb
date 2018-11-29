2b019d873b7a51fa1ef848e1ec925efae0abed2e
SK2929 <sk@koyamashoutairanoMacBook-Pro.local>
2018-11-29 16:40:42 +0900

    Edit-users_controller.rb
    指摘事項：current_user取得しないようにしましょう
    
    Where.notを使用して、ログイン済みのユーザーは表示しないように変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
 def index
-  @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+  @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(id: current_user.id)
   respond_to do |format|
     format.html
     format.json
@@ -20,3 +20,4 @@ def update
 end
 
 end
+

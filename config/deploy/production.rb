189d7476d16642d83bda968e0435a3194c7a780c
SK2929 <sk@koyamashoutairanoMacBook-Pro.local>
2018-12-05 20:14:39 +0900

    edit-Capistrano
    キャピストラノ

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -61,27 +61,3 @@
 #   }
 
 server '18.218.67.95', user: 'ec2-user', roles: %w{app db web}
-
-lock 'capistrano 3.11.0'
-
-set :application, 'chat-space'
-set :repo_url,  'git@github.com:SK2929/chat-space.git'
-
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-
-set :rbenv_type, :user
-set :rbenv_ruby, '2.3.1'
-
-set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/qw21op09[cd-55.pem']
-
-set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
-set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
-set :keep_releases, 5
-
-after 'deploy:publishing', 'deploy:restart'
-namespace :deploy do
-  task :restart do
-    invoke 'unicorn:restart'
-  end
-end

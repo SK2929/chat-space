93733a0a90fc38c28919aca4290cfc07bfddd668
SK2929 <sk@koyamashoutairanoMacBook-Pro.local>
2018-12-05 20:25:46 +0900

    Edit

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,7 +38,6 @@ set :repo_url, "git@github.com:SK2929/chat-space.git"
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 
-
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
